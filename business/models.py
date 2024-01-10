5fc8a4cea0a140314e67ae21b90d5050b6aec4d8
cloudrohu <cloudrohu@gmail.com>
2024-01-10 15:52:55 +0530

    socia_site

--- business/models.py
+++ business/models.py
@@ -195,6 +195,7 @@ class SocialSite(models.Model):
 
 class SocialLink(models.Model):
     company = models.ForeignKey(Company, on_delete=models.CASCADE,null=True,blank=True) #many to one relation with Brand
+    socia_site = models.ForeignKey(SocialSite, on_delete=models.CASCADE,null=True,blank=True) #many to one relation with Brand
     link = models.CharField(max_length=50,unique=True)    
     create_at=models.DateTimeField(auto_now_add=True)
     update_at=models.DateTimeField(auto_now=True)
